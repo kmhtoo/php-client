50ddeee47c9b8c8287366ba86afc298d4f5f33b3
gedi <gediminas.morkevicius@gmail.com>
2012-04-12 18:49:44 +0300

    [JIR-439] adapt sync tests

--- tests/Jirafe/Api/Collection/ResourcesTest.php
+++ tests/Jirafe/Api/Collection/ResourcesTest.php
@@ -29,6 +29,43 @@ class Jirafe_Api_Collection_ResourcesTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
+    public function shouldBeAbleToSyncResourcesIncludingApplication()
+    {
+        $sitesToSync = array(
+            array('description' => 'site1', 'url' => 'http://site1'),
+            array('description' => 'site1', 'url' => 'http://site2')
+        );
+        $usersToSync = array(
+            array('email' => 'everzet@knplabs.com', 'username' => 'everzet'),
+            array('email' => 'vjousse@knplabs.com', 'username' => 'vjousse')
+        );
+
+        $params = array(
+            'opt_in' => true,
+            'platform_type' => 'magento',
+            'platform_version' => '1.0.0',
+            'plugin_version' => '0.1.0',
+        );
+
+        $this->clientMock
+            ->expects($this->once())
+            ->method('post')
+            ->with('applications/41/resources', array(), array(
+                'sites' => $sitesToSync,
+                'users' => $usersToSync,
+                'opt_in' => true,
+                'platform_type' => 'magento',
+                'platform_version' => '1.0.0',
+                'plugin_version' => '0.1.0',
+            ))
+            ->will($this->returnValue(new Jirafe_HttpConnection_Response('"hash"', array(), 0, '')));
+
+        $this->assertEquals('hash', $this->resources->sync($sitesToSync, $usersToSync, $params));
+    }
+
+    /**
+     * @test
+     */
     public function shouldBeAbleToSyncResources()
     {
         $sitesToSync = array(
